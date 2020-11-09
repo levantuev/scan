fb50b8a6933a1f5d2b9881eac7c80002175a5067
Maybe <levantuev@gmail.com>
2020-11-09 17:45:43 +0300

    qwerrt

--- app/Http/Requests/ContactRequest.php
+++ app/Http/Requests/ContactRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request as BaseRequest;
 
 class ContactRequest extends FormRequest
 {
@@ -13,7 +14,7 @@ class ContactRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
