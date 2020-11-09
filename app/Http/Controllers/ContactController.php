2f77efa4260143046792941b7db4521040bbce93
Maybe <levantuev@gmail.com>
2020-11-09 17:40:33 +0300

    qwerrt

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -8,9 +8,7 @@ use App\Http\Requests\ContactRequest;
 class ContactController extends Controller
 {
     public function submit(ContactRequest $req) {
-      $validation = $req->validate([
-        'subject' => 'required|min:5|max:50',
-        'message' => 'required|min:15|max:500'
-      ]);
+      return "LOL";
+
     }
 }
